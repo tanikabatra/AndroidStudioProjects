89c4b2c9f06eab248c2072058fc37e95ea0e6b51
Tanika Batra <tanikabatra01@gmail.com>
2017-05-02 19:58:40 +0530

    Initial commit

---
+++ app/src/main/java/com/training/uploaddemo/Util.java
@@ -0,0 +1,15 @@
+package com.training.uploaddemo;
+
+/**
+ * Created by GOD on 5/2/2017.
+ */
+
+public class Util {
+
+    public static final String URI = "http://tanikabatra.esy.es/training/" ;
+
+    public static final String UPLOAD_PHP = URI + "upload.php";
+
+    public static final String UPLOAD_KEY = "image";
+    public static final String TAG = "MY MESSAGE";
+}
